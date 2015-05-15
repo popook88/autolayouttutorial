4a79e490ad856223170bd42963748f8b6c34d9fd
Kevin Kane <popook88@gmail.com>
2015-05-15 17:18:18 -0600

    Initial Commit

---
+++ AutoLayoutTutorial/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  AutoLayoutTutorial
+//
+//  Created by Kevin Kane on 5/15/15.
+//  Copyright (c) 2015 KevinKane. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
